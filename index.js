ec3646374d61a986c35aec88730c9ae9a1b521fc
Maria <maryallbond@gmail.com>
2024-05-08 21:05:50 +0300

    made edits for the main enter

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const root = {
   fieldResult: document.querySelector(".head-result"),
   fieldHistory: document.querySelector(".head-history"),
   BoxForResult: document.querySelector(".head-flex_box"),
+  equals: document.querySelector(".head-equals"),
 };
 root.interfaceEl.addEventListener("click", enterInResult);
 function enterInResult(event) {
@@ -22,22 +23,23 @@ function enterInResult(event) {
     root.fieldResult.innerHTML = "0";
     root.fieldHistory.innerHTML = "";
     enterStr = "";
+    hiddenEquals();
     return;
   }
   if (element === "=") {
     let result;
-    let processingStr;
+    let processingStr = enterStr;
     if (enterStr.includes("×")) {
-      processingStr = enterStr.replace("×", "*");
+      processingStr = processingStr.replace("×", "*");
     }
     if (enterStr.includes("÷")) {
-      processingStr = enterStr.replace("÷", "/");
+      processingStr = processingStr.replace("÷", "/");
     }
-    result = eval(processingStr);
-    root.BoxForResult.insertAdjacentHTML(
-      "afterbegin",
-      `<p class="head-equals">=</p>`
-    );
+    result = String(eval(processingStr))
+      .split("")
+      .filter((item, index) => index < 12)
+      .join("");
+    root.equals.removeAttribute("hidden");
     root.fieldHistory.innerHTML = enterStr; // вивід історії
     root.fieldResult.innerHTML = result;
     enterStr = result;
@@ -45,8 +47,16 @@ function enterInResult(event) {
   }
   if (element === ".") {
   }
+  hiddenEquals();
   enterStr += element;
+  root.equals.setAttribute("hidden", "");
   root.fieldResult.innerHTML = enterStr;
+}
+
+function hiddenEquals() {
+  if (!root.equals.hasAttribute("hidden")) {
+    root.equals.setAttribute("hidden", "");
+  }
 }
 
 function checked(elem) {
